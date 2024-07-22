4ce56d93e5c1a4710637355469e28139d9b675e4
Atatakai <atatakai@Atatakais-MacBook-Pro.local>
2024-07-22 17:45:52 +0400

    Add entities for emissions to devs

--- src/dispenser.ts
+++ src/dispenser.ts
@@ -1,66 +1,113 @@
+import { BigInt } from "@graphprotocol/graph-ts";
 import {
   IncentivesClaimed as IncentivesClaimedEvent,
   OwnerUpdated as OwnerUpdatedEvent,
   TokenomicsUpdated as TokenomicsUpdatedEvent,
   TreasuryUpdated as TreasuryUpdatedEvent,
-} from "../generated/Dispenser/Dispenser"
+} from "../generated/Dispenser/Dispenser";
 import {
+  DevIncentive,
+  Epoch,
   IncentivesClaimed,
   OwnerUpdated,
   TokenomicsUpdated,
   TreasuryUpdated,
-} from "../generated/schema"
+} from "../generated/schema";
 
 export function handleIncentivesClaimed(event: IncentivesClaimedEvent): void {
   let entity = new IncentivesClaimed(
-    event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.owner = event.params.owner
-  entity.reward = event.params.reward
-  entity.topUp = event.params.topUp
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  );
+  entity.owner = event.params.owner;
+  entity.reward = event.params.reward;
+  entity.topUp = event.params.topUp;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
+
+  // Update Epoch entity with devIncentive
+  let blockNumber = event.block.number;
+  let epoch: Epoch | null = null;
+
+  // Find the current epoch based on the block number
+  let epochCounter = 1;
+  while (true) {
+    let epochId = epochCounter.toString();
+    let currentEpoch = Epoch.load(epochId);
+    if (!currentEpoch) {
+      break;
+    }
+
+    // Check if IncentivesClaimed event happened during currentEpoch
+    if (
+      currentEpoch.startBlock.le(blockNumber) &&
+      (currentEpoch.endBlock === null || currentEpoch.endBlock!.ge(blockNumber))
+    ) {
+      epoch = currentEpoch;
+      break;
+    }
+
+    epochCounter++;
+  }
+
+  if (epoch) {
+    let devIncentive = new DevIncentive(event.transaction.hash.toHex());
+    devIncentive.epoch = epoch.id;
+    devIncentive.owner = event.params.owner;
+    devIncentive.reward = event.params.reward;
+    devIncentive.topUp = event.params.topUp;
+    devIncentive.save();
+
+    // Update the total topUp in the epoch
+    if (!epoch.devIncentivesTotalTopUp) {
+      epoch.devIncentivesTotalTopUp = event.params.topUp;
+    } else {
+      epoch.devIncentivesTotalTopUp = epoch.devIncentivesTotalTopUp!.plus(
+        event.params.topUp
+      );
+    }
+    epoch.save();
+  }
 }
 
 export function handleOwnerUpdated(event: OwnerUpdatedEvent): void {
   let entity = new OwnerUpdated(
-    event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.owner = event.params.owner
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  );
+  entity.owner = event.params.owner;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleTokenomicsUpdated(event: TokenomicsUpdatedEvent): void {
   let entity = new TokenomicsUpdated(
-    event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.tokenomics = event.params.tokenomics
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  );
+  entity.tokenomics = event.params.tokenomics;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleTreasuryUpdated(event: TreasuryUpdatedEvent): void {
   let entity = new TreasuryUpdated(
-    event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.treasury = event.params.treasury
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  );
+  entity.treasury = event.params.treasury;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
--- src/tokenomics.ts
+++ src/tokenomics.ts
@@ -1,3 +1,4 @@
+import { BigInt } from "@graphprotocol/graph-ts";
 import {
   AgentRegistryUpdated as AgentRegistryUpdatedEvent,
   ComponentRegistryUpdated as ComponentRegistryUpdatedEvent,
@@ -7,6 +8,7 @@ import {
   EffectiveBondUpdated as EffectiveBondUpdatedEvent,
   EpochLengthUpdated as EpochLengthUpdatedEvent,
   EpochSettled as EpochSettledEvent,
+  EpochSettled1 as EpochSettledOldEvent,
   IDFUpdated as IDFUpdatedEvent,
   IncentiveFractionsUpdateRequested as IncentiveFractionsUpdateRequestedEvent,
   IncentiveFractionsUpdated as IncentiveFractionsUpdatedEvent,
@@ -19,7 +21,7 @@ import {
   TokenomicsParametersUpdateRequested as TokenomicsParametersUpdateRequestedEvent,
   TokenomicsParametersUpdated as TokenomicsParametersUpdatedEvent,
   TreasuryUpdated as TreasuryUpdatedEvent,
-} from "../generated/Tokenomics/Tokenomics"
+} from "../generated/Tokenomics/Tokenomics";
 import {
   AgentRegistryUpdated,
   ComponentRegistryUpdated,
@@ -41,309 +43,364 @@ import {
   TokenomicsParametersUpdateRequested,
   TokenomicsParametersUpdated,
   TreasuryUpdated,
-} from "../generated/schema"
+  Epoch,
+} from "../generated/schema";
 
 export function handleAgentRegistryUpdated(
-  event: AgentRegistryUpdatedEvent,
+  event: AgentRegistryUpdatedEvent
 ): void {
   let entity = new AgentRegistryUpdated(
-    event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.agentRegistry = event.params.agentRegistry
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  );
+  entity.agentRegistry = event.params.agentRegistry;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleComponentRegistryUpdated(
-  event: ComponentRegistryUpdatedEvent,
+  event: ComponentRegistryUpdatedEvent
 ): void {
   let entity = new ComponentRegistryUpdated(
-    event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.componentRegistry = event.params.componentRegistry
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  );
+  entity.componentRegistry = event.params.componentRegistry;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleDepositoryUpdated(event: DepositoryUpdatedEvent): void {
   let entity = new DepositoryUpdated(
-    event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.depository = event.params.depository
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  );
+  entity.depository = event.params.depository;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleDispenserUpdated(event: DispenserUpdatedEvent): void {
   let entity = new DispenserUpdated(
-    event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.dispenser = event.params.dispenser
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  );
+  entity.dispenser = event.params.dispenser;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleDonatorBlacklistUpdated(
-  event: DonatorBlacklistUpdatedEvent,
+  event: DonatorBlacklistUpdatedEvent
 ): void {
   let entity = new DonatorBlacklistUpdated(
-    event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.blacklist = event.params.blacklist
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  );
+  entity.blacklist = event.params.blacklist;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleEffectiveBondUpdated(
-  event: EffectiveBondUpdatedEvent,
+  event: EffectiveBondUpdatedEvent
 ): void {
   let entity = new EffectiveBondUpdated(
-    event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.epochNumber = event.params.epochNumber
-  entity.effectiveBond = event.params.effectiveBond
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  );
+  entity.epochNumber = event.params.epochNumber;
+  entity.effectiveBond = event.params.effectiveBond;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleEpochLengthUpdated(event: EpochLengthUpdatedEvent): void {
   let entity = new EpochLengthUpdated(
-    event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.epochLen = event.params.epochLen
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  );
+  entity.epochLen = event.params.epochLen;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleEpochSettled(event: EpochSettledEvent): void {
   let entity = new EpochSettled(
-    event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.epochCounter = event.params.epochCounter
-  entity.treasuryRewards = event.params.treasuryRewards
-  entity.accountRewards = event.params.accountRewards
-  entity.accountTopUps = event.params.accountTopUps
-  entity.effectiveBond = event.params.effectiveBond
-  entity.returnedStakingIncentive = event.params.returnedStakingIncentive
-  entity.totalStakingIncentive = event.params.totalStakingIncentive
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  );
+  entity.epochCounter = event.params.epochCounter;
+  entity.treasuryRewards = event.params.treasuryRewards;
+  entity.accountRewards = event.params.accountRewards;
+  entity.accountTopUps = event.params.accountTopUps;
+  entity.effectiveBond = event.params.effectiveBond;
+  entity.returnedStakingIncentive = event.params.returnedStakingIncentive;
+  entity.totalStakingIncentive = event.params.totalStakingIncentive;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
+
+  // Epoch entity
+  let epoch = new Epoch(event.params.epochCounter.toString());
+
+  epoch.counter = event.params.epochCounter.toI32();
+  epoch.startBlock = event.block.number;
+  epoch.endBlock = null;
+
+  // Find the previous epoch and set its end block
+  if (epoch.counter > 1) {
+    let previousEpochId = (epoch.counter - 1).toString();
+    let previousEpoch = Epoch.load(previousEpochId);
+    if (previousEpoch) {
+      previousEpoch.endBlock = event.block.number.minus(BigInt.fromI32(1));
+      previousEpoch.save();
+    }
+  }
+
+  epoch.save();
+}
+
+export function handleEpochSettledOld(event: EpochSettledOldEvent): void {
+  let entity = new EpochSettled(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  );
+  entity.epochCounter = event.params.epochCounter;
+  entity.treasuryRewards = event.params.treasuryRewards;
+  entity.accountRewards = event.params.accountRewards;
+  entity.accountTopUps = event.params.accountTopUps;
+
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
+
+  entity.save();
+
+  // Epoch entity
+  let epoch = new Epoch(event.params.epochCounter.toString());
+
+  epoch.counter = event.params.epochCounter.toI32();
+  epoch.startBlock = event.block.number;
+  epoch.endBlock = null;
+
+  // Find the previous epoch and set its end block
+  if (epoch.counter > 1) {
+    let previousEpochId = (epoch.counter - 1).toString();
+    let previousEpoch = Epoch.load(previousEpochId);
+    if (previousEpoch) {
+      previousEpoch.endBlock = event.block.number.minus(BigInt.fromI32(1));
+      previousEpoch.save();
+    }
+  }
+
+  epoch.save();
 }
 
 export function handleIDFUpdated(event: IDFUpdatedEvent): void {
   let entity = new IDFUpdated(
-    event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.idf = event.params.idf
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  );
+  entity.idf = event.params.idf;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleIncentiveFractionsUpdateRequested(
-  event: IncentiveFractionsUpdateRequestedEvent,
+  event: IncentiveFractionsUpdateRequestedEvent
 ): void {
   let entity = new IncentiveFractionsUpdateRequested(
-    event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.epochNumber = event.params.epochNumber
-  entity.rewardComponentFraction = event.params.rewardComponentFraction
-  entity.rewardAgentFraction = event.params.rewardAgentFraction
-  entity.maxBondFraction = event.params.maxBondFraction
-  entity.topUpComponentFraction = event.params.topUpComponentFraction
-  entity.topUpAgentFraction = event.params.topUpAgentFraction
-  entity.stakingFraction = event.params.stakingFraction
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  );
+  entity.epochNumber = event.params.epochNumber;
+  entity.rewardComponentFraction = event.params.rewardComponentFraction;
+  entity.rewardAgentFraction = event.params.rewardAgentFraction;
+  entity.maxBondFraction = event.params.maxBondFraction;
+  entity.topUpComponentFraction = event.params.topUpComponentFraction;
+  entity.topUpAgentFraction = event.params.topUpAgentFraction;
+  entity.stakingFraction = event.params.stakingFraction;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleIncentiveFractionsUpdated(
-  event: IncentiveFractionsUpdatedEvent,
+  event: IncentiveFractionsUpdatedEvent
 ): void {
   let entity = new IncentiveFractionsUpdated(
-    event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.epochNumber = event.params.epochNumber
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  );
+  entity.epochNumber = event.params.epochNumber;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleOwnerUpdated(event: OwnerUpdatedEvent): void {
   let entity = new OwnerUpdated(
-    event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.owner = event.params.owner
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  );
+  entity.owner = event.params.owner;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleServiceRegistryUpdated(
-  event: ServiceRegistryUpdatedEvent,
+  event: ServiceRegistryUpdatedEvent
 ): void {
   let entity = new ServiceRegistryUpdated(
-    event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.serviceRegistry = event.params.serviceRegistry
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  );
+  entity.serviceRegistry = event.params.serviceRegistry;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleStakingParamsUpdateRequested(
-  event: StakingParamsUpdateRequestedEvent,
+  event: StakingParamsUpdateRequestedEvent
 ): void {
   let entity = new StakingParamsUpdateRequested(
-    event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.epochNumber = event.params.epochNumber
-  entity.maxStakingIncentive = event.params.maxStakingIncentive
-  entity.minStakingWeight = event.params.minStakingWeight
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  );
+  entity.epochNumber = event.params.epochNumber;
+  entity.maxStakingIncentive = event.params.maxStakingIncentive;
+  entity.minStakingWeight = event.params.minStakingWeight;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleStakingParamsUpdated(
-  event: StakingParamsUpdatedEvent,
+  event: StakingParamsUpdatedEvent
 ): void {
   let entity = new StakingParamsUpdated(
-    event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.epochNumber = event.params.epochNumber
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  );
+  entity.epochNumber = event.params.epochNumber;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleStakingRefunded(event: StakingRefundedEvent): void {
   let entity = new StakingRefunded(
-    event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.epochNumber = event.params.epochNumber
-  entity.amount = event.params.amount
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  );
+  entity.epochNumber = event.params.epochNumber;
+  entity.amount = event.params.amount;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleTokenomicsImplementationUpdated(
-  event: TokenomicsImplementationUpdatedEvent,
+  event: TokenomicsImplementationUpdatedEvent
 ): void {
   let entity = new TokenomicsImplementationUpdated(
-    event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.implementation = event.params.implementation
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  );
+  entity.implementation = event.params.implementation;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleTokenomicsParametersUpdateRequested(
-  event: TokenomicsParametersUpdateRequestedEvent,
+  event: TokenomicsParametersUpdateRequestedEvent
 ): void {
   let entity = new TokenomicsParametersUpdateRequested(
-    event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.epochNumber = event.params.epochNumber
-  entity.devsPerCapital = event.params.devsPerCapital
-  entity.codePerDev = event.params.codePerDev
-  entity.epsilonRate = event.params.epsilonRate
-  entity.epochLen = event.params.epochLen
-  entity.veOLASThreshold = event.params.veOLASThreshold
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  );
+  entity.epochNumber = event.params.epochNumber;
+  entity.devsPerCapital = event.params.devsPerCapital;
+  entity.codePerDev = event.params.codePerDev;
+  entity.epsilonRate = event.params.epsilonRate;
+  entity.epochLen = event.params.epochLen;
+  entity.veOLASThreshold = event.params.veOLASThreshold;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleTokenomicsParametersUpdated(
-  event: TokenomicsParametersUpdatedEvent,
+  event: TokenomicsParametersUpdatedEvent
 ): void {
   let entity = new TokenomicsParametersUpdated(
-    event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.epochNumber = event.params.epochNumber
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  );
+  entity.epochNumber = event.params.epochNumber;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleTreasuryUpdated(event: TreasuryUpdatedEvent): void {
   let entity = new TreasuryUpdated(
-    event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.treasury = event.params.treasury
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  );
+  entity.treasury = event.params.treasury;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
