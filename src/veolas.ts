cdee3590679429b44f02a55f571943a210977297
axatbhardwaj <axatbhardwaj@outlook.com>
2025-07-03 15:14:30 +0530

    chore: remove VeOLASLock entity and associated deposit handling from schema and code

--- src/veolas.ts
+++ src/veolas.ts
@@ -1,22 +1,8 @@
-import { Transfer as TransferEvent, Deposit as DepositEvent } from "../generated/veOLAS/veOLAS";
-import { TokenHolder, VeOLASLock } from "../generated/schema";
+import { Transfer as TransferEvent } from "../generated/veOLAS/veOLAS";
+import { TokenHolder } from "../generated/schema";
 import { BigInt } from "@graphprotocol/graph-ts";
 
 const BIGINT_ZERO = BigInt.fromI32(0);
-
-export function handleDeposit(event: DepositEvent): void {
-  let lockId = event.transaction.hash.toHexString().concat("-").concat(event.logIndex.toString());
-  let lock = new VeOLASLock(lockId);
-  lock.account = event.params.account;
-  lock.amount = event.params.amount;
-  lock.unlockTime = event.params.locktime;
-  lock.depositType = event.params.depositType;
-  lock.timestamp = event.params.ts;
-  lock.isActive = event.params.locktime.gt(event.block.timestamp);
-  lock.blockNumber = event.block.number;
-  lock.blockTimestamp = event.block.timestamp;
-  lock.save();
-}
 
 export function handleTransfer(event: TransferEvent): void {
   // Generic balance tracking
