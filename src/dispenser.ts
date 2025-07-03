e98a99bdadc1db69bc4dc5703de6492519d7f709
axatbhardwaj <axatbhardwaj@outlook.com>
2025-07-03 15:22:37 +0530

    chore : restored original dispenser.ts

--- src/dispenser.ts
+++ src/dispenser.ts
@@ -1,5 +1,5 @@
 import { Address, BigInt, Bytes } from "@graphprotocol/graph-ts";
-import { IncentivesClaimed as IncentivesClaimedEvent } from "../generated/DispenserV1/DispenserV1";
+import { IncentivesClaimed as IncentivesClaimedEvent } from "../generated/Dispenser/Dispenser";
 import {
   DispenserV2,
   AddNomineeHash as AddNomineeHashEvent,
