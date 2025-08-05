28f51b0d60251c6f53153c412ec3f96193ffc458
axatbhardwaj <axatbhardwaj@outlook.com>
2025-08-05 22:39:07 +0530

    fix: update network configuration to xdai and convert amounts from Wei to USD in event handlers

--- subgraphs/new-mech-fees/new-native-mech-fees-gnosis/src/mapping.ts
+++ subgraphs/new-mech-fees/new-native-mech-fees-gnosis/src/mapping.ts
@@ -6,17 +6,23 @@ import {
   Withdraw
 } from "../generated/BalanceTrackerFixedPriceNative/BalanceTrackerFixedPriceNative"
 import { Mech } from "../generated/schema"
-import { BURN_ADDRESS_MECH_FEES_GNOSIS } from "../../../../shared/constants"
+import { BURN_ADDRESS_MECH_FEES_GNOSIS, ETH_DECIMALS } from "../../../../shared/constants"
 import { updateTotalFeesIn, updateTotalFeesOut } from "./utils"
 
 const BURN_ADDRESS = Address.fromString(BURN_ADDRESS_MECH_FEES_GNOSIS);
 
+function convertWeiToUsd(amountInWei: BigInt): BigInt {
+  const ethDivisor = BigInt.fromI32(10).pow(ETH_DECIMALS as u8);
+  return amountInWei.div(ethDivisor);
+}
 
 export function handleMechBalanceAdjustedForNative(event: MechBalanceAdjusted): void {
-  const earningsAmount = event.params.deliveryRate;
+  const earningsAmountWei = event.params.deliveryRate;
   const mechId = event.params.mech.toHex();
 
-  updateTotalFeesIn(earningsAmount);
+  const earningsAmountUsd = convertWeiToUsd(earningsAmountWei);
+
+  updateTotalFeesIn(earningsAmountUsd);
 
   let mech = Mech.load(mechId);
   if (mech == null) {
@@ -24,24 +30,26 @@ export function handleMechBalanceAdjustedForNative(event: MechBalanceAdjusted): 
     mech.totalFeesIn = BigInt.fromI32(0);
     mech.totalFeesOut = BigInt.fromI32(0);
   }
-  mech.totalFeesIn = mech.totalFeesIn.plus(earningsAmount);
+  mech.totalFeesIn = mech.totalFeesIn.plus(earningsAmountUsd);
   mech.save();
 }
 
 export function handleWithdrawForNative(event: Withdraw): void {
   const recipientAddress = event.params.account;
-  const withdrawalAmount = event.params.amount;
+  const withdrawalAmountWei = event.params.amount;
   const mechId = recipientAddress.toHex();
 
   if (recipientAddress.equals(BURN_ADDRESS)) {
     return;
   }
 
-  updateTotalFeesOut(withdrawalAmount);
+  const withdrawalAmountUsd = convertWeiToUsd(withdrawalAmountWei);
+
+  updateTotalFeesOut(withdrawalAmountUsd);
 
   const mech = Mech.load(mechId);
   if (mech != null) {
-    mech.totalFeesOut = mech.totalFeesOut.plus(withdrawalAmount);
+    mech.totalFeesOut = mech.totalFeesOut.plus(withdrawalAmountUsd);
     mech.save();
   }
 }
