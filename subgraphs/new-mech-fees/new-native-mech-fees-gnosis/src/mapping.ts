dd619690a08a4abd878772c40a8d317e6ac52345
axatbhardwaj <axatbhardwaj@outlook.com>
2025-08-05 20:58:10 +0530

    refactor : dir change

---
+++ subgraphs/new-mech-fees/new-native-mech-fees-gnosis/src/mapping.ts
@@ -0,0 +1,47 @@
+// src/mapping.ts
+
+import { Address, BigInt } from "@graphprotocol/graph-ts"
+import {
+  MechBalanceAdjusted,
+  Withdraw
+} from "../generated/BalanceTrackerFixedPriceNative/BalanceTrackerFixedPriceNative"
+import { Mech } from "../generated/schema"
+import { BURN_ADDRESS_MECH_FEES_GNOSIS } from "../../../../shared/constants"
+import { updateTotalFeesIn, updateTotalFeesOut } from "./utils"
+
+const BURN_ADDRESS = Address.fromString(BURN_ADDRESS_MECH_FEES_GNOSIS);
+
+
+export function handleMechBalanceAdjustedForNative(event: MechBalanceAdjusted): void {
+  const earningsAmount = event.params.deliveryRate;
+  const mechId = event.params.mech.toHex();
+
+  updateTotalFeesIn(earningsAmount);
+
+  let mech = Mech.load(mechId);
+  if (mech == null) {
+    mech = new Mech(mechId);
+    mech.totalFeesIn = BigInt.fromI32(0);
+    mech.totalFeesOut = BigInt.fromI32(0);
+  }
+  mech.totalFeesIn = mech.totalFeesIn.plus(earningsAmount);
+  mech.save();
+}
+
+export function handleWithdrawForNative(event: Withdraw): void {
+  const recipientAddress = event.params.account;
+  const withdrawalAmount = event.params.amount;
+  const mechId = recipientAddress.toHex();
+
+  if (recipientAddress.equals(BURN_ADDRESS)) {
+    return;
+  }
+
+  updateTotalFeesOut(withdrawalAmount);
+
+  const mech = Mech.load(mechId);
+  if (mech != null) {
+    mech.totalFeesOut = mech.totalFeesOut.plus(withdrawalAmount);
+    mech.save();
+  }
+}
